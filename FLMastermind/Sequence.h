919329af98f5d2911f04ebfca79803142c4b26fe
lwidajew <lwidajew@umich.edu>
2026-02-23 10:27:41 -0500

    Created game loop

--- FLMastermind/Sequence.h
+++ FLMastermind/Sequence.h
@@ -24,7 +24,6 @@ public:
 
 private:
 	Node* head;
-	Node* tail;
 };
 
 #endif
--- FLMastermind/main.cpp
+++ FLMastermind/main.cpp
@@ -8,8 +8,31 @@ int main() {
 	cout << "-----------------------------------------------------------------------" << endl << endl;
 
 	Mastermind game;
+	int turnNum = 0;
+	int maxAttempts = 10;
+	bool won = false;
 
 	game.printColors();
 	game.makeSequence();
-	game.getPlayerGuess();
+
+	// start game loop
+	while (!won && turnNum < maxAttempts) {
+		cout << "Attempt #" << turnNum + 1 << endl;
+
+		game.getPlayerGuess();
+		won = game.compareSequences();
+
+		turnNum++;
+	};
+
+	if (won) {
+		// player won print out win screen
+		cout << "\nYou have cracked the code and won!" << endl;
+	}
+	else {
+		// player lost via reaching max attempts
+		cout << "\nYou have lost! You've used all your attempts!" << endl;
+		// print out the secret code that was not guessed by the player
+		game.printSecretCode();
+	};
 };
